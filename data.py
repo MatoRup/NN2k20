182bf74e89ec7c730250870dda82426cfa801b99
MatoRup <matevz.rupnik@gmail.com>
2020-05-13 08:54:01 +0200

    FFT smoothing function

--- data.py
+++ data.py
@@ -8,6 +8,26 @@ from pandas import ExcelWriter
 from pandas import ExcelFile
 from sklearn.preprocessing import MinMaxScaler
 
+def FFT_smoothing(y,smootnes=0.01):
+  """
+  Return the data without trend and trend
+  """
+  mena=y.mean()
+  y = y - mena
+  freqs = np.fft.fftshift(np.fft.fftfreq(len(y)))
+  fftVar = np.fft.fftshift(np.fft.fft(y))
+  fft_filtered = fftVar.copy()
+  abs=np.abs(freqs)
+  add=np.max(abs[np.nonzero(abs)])+np.min(abs[np.nonzero(abs)])
+  limit=np.min(abs[np.nonzero(abs)])+add*smootnes
+  #Here I delete low frequencies
+  fft_filtered[np.abs(freqs)>limit]=0
+  trend = np.fft.ifft(np.fft.ifftshift(fft_filtered))
+  without_trend=y-trend+mena
+  #Returning just rela part I hope this is correct
+  return(without_trend.real,trend.real)
+
+
 M3Year = pd.read_excel('M3C.xls',sheet_name='M3Year')
 M3Quart= pd.read_excel('M3C.xls',sheet_name='M3Quart')
 M3Month = pd.read_excel('M3C.xls',sheet_name='M3Month')
